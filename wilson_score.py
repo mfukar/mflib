66bae1380859af8d92f92b9fb5cfda0f293fe3b6
Michael Foukarakis <michael.foukarakis@bmw-carit.de>
2017-09-01 16:40:27 +0300

    wilson: Typo fix
    Signed-off-by: Michael Foukarakis <michael.foukarakis@bmw-carit.de>

--- wilson_score.py
+++ wilson_score.py
@@ -3,9 +3,9 @@
 # @author      Michael Foukarakis
 # @version     0.1
 # @date        Created:     Fri Sep 01, 2017 10:19 EEST
-#              Last Update: Fri Sep 01, 2017 11:06 EEST
+#              Last Update: Fri Sep 01, 2017 16:40 EEST
 #------------------------------------------------------------------------
-# Description: <+description+>
+# Description: Wilson score interval
 #------------------------------------------------------------------------
 # History:     <+history+>
 # TODO:        <+missing features+>
@@ -18,11 +18,12 @@ from math import sqrt
 def ci_lower_bound(positive, n, confidence):
     """Returns the lower-bound Wilson score for a given confidence interval.
     positive (int): Number of positive events
-    n (int): Total number of trialas
+    n (int): Total number of trials
     confidence (float): The percentage of confidence that the 'true' value of
                         positive events is within this confidence interval"""
-    if n == 0: return 0
+    if n == 0:
+        return 0
     z = norm.ppf(1 - (1 - confidence) / 2.0)
     phat = 1.0 * pos / n
-    return (phat + z*z/(2*n) - z * sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / (1 + z*z / n)
+    return (phat + z*z / (2 * n) - z * sqrt((phat * (1 - phat) + z * z / (4 * n)) / n)) / (1 + z*z / n)
 
