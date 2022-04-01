1df725616872797f69471d8ebb7b4f2648a2d352
mfukar <foukarakis.michael@gmail.com>
2022-04-01 19:21:33 +0200

    plotter: Fix
    This must have worked back in the stone age
    
    Signed-off-by: mfukar <foukarakis.michael@gmail.com>

--- plotter.py
+++ plotter.py
@@ -1,24 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # @file        /Users/mfukar/src/mflib/plotter.py
 # @author      Michael Foukarakis
 # @version     <+version+>
 # @date        Created:     Sun Jul 05, 2015 17:02 EEST
-#              Last Update: Sun Jul 05, 2015 17:04 EEST
+#              Last Update: Fri Apr 01, 2022 19:21 W. Europe Daylight Time
 #------------------------------------------------------------------------
 # Description: Various plotting & visualisation routines
 #------------------------------------------------------------------------
-# History:     <+history+>
-# TODO:        <+missing features+>
-#------------------------------------------------------------------------
 # -*- coding: utf-8 -*-
 #------------------------------------------------------------------------
+from math import floor
 
-def plot_counter(collection, width=120):
+def plot_counter(collection, width=100):
     """Plot a histogram of a collections.Counter in text:
     """
-    longest_key = max( len(key for key in collection))
+    longest_key = max(len(key) for key in collection)
     graph_width = width - longest_key - 2
     widest = collection.most_common(1)[0][1]
     scale = graph_width / floor(widest)
-    for k, v in sorted(collection.items()):
-        print('{}: {}'.format(k, int(v * scale) * '='))
+    for k, v in collection.most_common():
+        print('{}: {} {}'.format(k, (1 + int(v * scale)) * '=', v))
